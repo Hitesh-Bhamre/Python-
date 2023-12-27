3745cddaef235149e7a40b82e976616aab0572a3
Hitesh Bhamre <bhamrehitesh1234@gmail.com>
2023-12-27 14:19:43 +0530

    Python Interview Questions

---
+++ practice.py
@@ -0,0 +1,14 @@
+
+# Pthon Practice
+
+# Q1
+
+# Add two num (with user-input)
+
+num1 = float(input("Enter a number here: "))
+num2 = float(input("Enter another number here: "))
+
+sum = num1 + num2
+
+print("the sum of two number is ", sum)
+
